9c072a7fde4c3d154c7f64f36b14fb249aad22ab
Herbal Jelly <133307262+HerbalJelly23@users.noreply.github.com>
2023-09-02 00:13:40 +0800

    Update Home.py

--- Streamlit_CASYSU_Web/Home.py
+++ Streamlit_CASYSU_Web/Home.py
@@ -19,7 +19,7 @@ st.caption('本站由[:green[Herbal Jelly]](https://github.com/HerbalJelly23)提
 st.header('协会简介')
 st.write(f'{os.path.dirname(os.path.abspath(__file__))}')
 with st.expander('点击以收起协会简介', expanded=1):
-    home_header_image = Image.open('./pages/pic002.png')
+    home_header_image = Image.open('./pages/pic001.png')
     st.image(home_header_image, caption='')
 
 
